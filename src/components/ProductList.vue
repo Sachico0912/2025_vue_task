38f7cb39ee5240725a39d11ceb8392f0ab05b4a8
Sachico <s104103045@gmail.com>
2025-09-02 17:43:51 +0800

    fix:add to cart button

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -22,13 +22,13 @@ const showNotification = inject('showNotification')
     <h2 class="mb-3">商品列表</h2>
     <div class="row">
       <div v-for="product in products" :key="product.id" class="col-md-4 mb-4">
-        <div class="card h-100">
+        <div class="card h-100 d-flex flex-column">
           <img :src="product.image" class="card-img-top" />
           <div class="card-body">
             <h5 class="card-title">{{ product.name }}</h5>
             <p class="card-text">{{ product.description }}</p>
             <p class="fw-bold text-primary">$ {{ product.price }}</p>
-            <button class="btn btn-success w-100" @click="handelAddCart(product)">
+            <button class="btn btn-success w-100 mt-auto" @click="handelAddCart(product)">
               加入購物車
             </button>
           </div>
